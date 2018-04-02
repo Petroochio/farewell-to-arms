eb98c8d70250cba1b98113d95061b35cf76eb18d
Peter Gyory <ptgyory@gmail.com>
2018-04-01 18:23:16 -0600

    Fix heroku port

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ io.on('connection', (socket) => {
     }
   });
 });
-
-http.listen(3000, () => {
+const port = process.env.PORT || 3000;
+http.listen(port, () => {
   console.log('listening on *:3000');
 });
